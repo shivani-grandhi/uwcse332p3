8672c9343dc5736c9f6d3dd0031363e49f1917e1
Shivani Grandhi <shivag2@uw.edu>
2023-02-23 23:17:01 -0800

    p3 ch 1

--- src/main/java/main/Parser.java
+++ src/main/java/main/Parser.java
@@ -2,15 +2,33 @@ package main;
 
 import cse332.exceptions.NotYetImplementedException;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Parser {
 
     /**
      * Parse an adjacency matrix into an adjacency list.
+     *
      * @param adjMatrix Adjacency matrix
      * @return Adjacency list
      */
-    public static Object parse(int[][] adjMatrix) {
-        throw new NotYetImplementedException();
+    public static ArrayList<HashMap<Integer, Integer>> parse(int[][] adjMatrix) {
+        int cost = 0;
+        int l = adjMatrix[0].length;
+        ArrayList<HashMap<Integer, Integer>> al = new ArrayList<HashMap<Integer, Integer>>(l);
+        for (int i=0; i<l; i++) {
+            al.add(i, new HashMap<Integer, Integer>());
+        }
+        for (int i = 0; i < l; i++) {
+            for (int j = 0; j < adjMatrix.length; j++) {
+                if (adjMatrix[i][j] != Integer.MAX_VALUE) {//!= 0 || adjMatrix[i][j] != Integer.MAX_VALUE) {//Integer.MAX_VALUE) {
+                    al.get(i).put(j, adjMatrix[i][j]);
+                }
+            }
+        }
+
+        return al;
     }
 
     /**
--- src/main/java/solvers/OutSequential.java
+++ src/main/java/solvers/OutSequential.java
@@ -1,17 +1,50 @@
 package solvers;
 
-import cse332.exceptions.NotYetImplementedException;
+
+import cse332.graph.GraphUtil;
 import cse332.interfaces.BellmanFordSolver;
 import main.Parser;
 
-import java.util.List;
+import java.util.*;
 
 public class OutSequential implements BellmanFordSolver {
 
     public List<Integer> solve(int[][] adjMatrix, int source) {
-        Object g = Parser.parse(adjMatrix);
+        ArrayList<HashMap<Integer, Integer>> adjList = Parser.parse(adjMatrix);
+        int numVertices = adjList.size();
 
-        throw new NotYetImplementedException();
+// Initialize D and P arrays
+        int[] D = new int[numVertices];
+        int[] P = new int[numVertices];
+        Arrays.fill(D, Integer.MAX_VALUE);
+        Arrays.fill(P, -1);
+        D[source] = 0;
+
+        for (int i = 0; i < numVertices; i++) {
+            // Create a new array for storing the updated values of D
+            int[] newD = new int[numVertices];
+            System.arraycopy(D, 0, newD, 0, numVertices); // Manually copy D to newD
+
+            // Relax all edges
+            for (int u = 0; u < numVertices; u++) {
+                HashMap<Integer, Integer> edges = adjList.get(u);
+                for (int v : edges.keySet()) {
+                    int weight = edges.get(v);
+                    if (newD[v] > D[u] + weight) {
+                        newD[v] = D[u] + weight;
+                        P[v] = u;
+                    }
+                }
+            }
+
+            // Update D with the new values
+            System.arraycopy(newD, 0, D, 0, numVertices);
+        }
+
+// Step 3: Finding negative-cost cycles (same for all implementations)
+        List<Integer> cycle = (LinkedList<Integer>) GraphUtil.getCycle(P);
+        return cycle;
+
     }
 
 }
